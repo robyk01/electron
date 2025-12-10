61bc1646c0b0cf87d3d203db21fcb1bf69ddaac8
denis <denislupastean51@gmail.com>
2025-12-10 20:04:54 +0200

    Adăugat funcționalitatea FULSCREEN si windowed la apasarea F11 sau ESC.

--- main.py
+++ main.py
@@ -4,25 +4,46 @@ import sys
 from src.settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, TITLU, COLORS
 from src.view.interface import draw_grid
 
-
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # CITIM DIMENSIUNEA MONITORULUI
+    info_monitor = pygame.display.Info()
+    MONITOR_W = info_monitor.current_w
+    MONITOR_H = info_monitor.current_h
+
+    FLAGS_FULLSCREEN = pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF
+    # pygame fullscreen scoate bara de sus
+    # pygame hwsurface face ca aplicatia sa ruleze pe GPU nu CPU
+    # DOUBLEBUF face doua ecrane, totul se deseneaza intre timp pe cel din spate si la flip ne arata rezultatul
+
+    screen = pygame.display.set_mode((MONITOR_W, MONITOR_H), FLAGS_FULLSCREEN)
+
     pygame.display.set_caption(TITLU)
     clock = pygame.time.Clock()
 
     running = True
+    is_fullscreen = True
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+            # Posibilitatea de a ieși din Fullscreen (ESC sau F11)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F11 or event.key == pygame.K_ESCAPE:
+                    is_fullscreen = not is_fullscreen
+
+                    if is_fullscreen:
+                        screen = pygame.display.set_mode((MONITOR_W, MONITOR_H), FLAGS_FULLSCREEN)
+                    else:
+                        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
             # (Aici vin adaugate clikcuri pe piese, mai dureaza)
 
-        # Desenare Ecran
+        # Desenare
         screen.fill(COLORS["BACKGROUND"])
-
-        # Apelez functia importata din interface.py
         draw_grid(screen)
 
         pygame.display.flip()
@@ -30,6 +51,7 @@ def main():
 
     pygame.quit()
     sys.exit()
+
 
 if __name__ == "__main__":
     main()
--- src/view/interface.py
+++ src/view/interface.py
@@ -1,8 +1,10 @@
 import pygame
-from src.settings import SCREEN_WIDTH, SCREEN_HEIGHT, GRID_SIZE, COLORS
-
+from src.settings import GRID_SIZE, COLORS
 def draw_grid(screen):
-    for x in range(0, SCREEN_WIDTH, GRID_SIZE):
-        for y in range(0, SCREEN_HEIGHT, GRID_SIZE):
-            # pygame.draw.circle(surface, color, center, radius) - documentatie pygame
+
+    latime_reala = screen.get_width()
+    inaltime_reala = screen.get_height()
+
+    for x in range(0, latime_reala, GRID_SIZE):
+        for y in range(0, inaltime_reala, GRID_SIZE):
             pygame.draw.circle(screen, COLORS["DOTS"], (x, y), 2)
